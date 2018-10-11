0efff126f16c8c627720b288b36536237fbe51f5
Diogo Cesa Rosa <diogo@fastmail.com>
2018-10-11 10:49:50 -0300

    Added base project.

---
+++ things/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  things
+//
+//  Created by Diogo on 11/10/2018.
+//  Copyright © 2018 Diogo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
