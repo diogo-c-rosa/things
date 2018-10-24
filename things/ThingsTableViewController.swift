0f779989640a342d7ba18573ec070b95678b51c2
Diogo Cesa Rosa <diogo@fastmail.com>
2018-10-24 09:27:31 -0300

    Added search bar.

--- things/ThingsTableViewController.swift
+++ things/ThingsTableViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ThingsTableViewController: UITableViewController {
-
+class ThingsTableViewController: UITableViewController, UISearchBarDelegate {
+    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     var things: [Thing] = []
+    var filteredThings: [Thing] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        searchBar.delegate = self
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -25,6 +30,7 @@ class ThingsTableViewController: UITableViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         getData()
+        filteredThings = things
         tableView.reloadData()
     }
     
@@ -43,7 +49,7 @@ class ThingsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return things.count
+        return filteredThings.count
     }
 
     func age(_ startDate: Date?, _ endDate: Date?) -> String? {
@@ -63,9 +69,9 @@ class ThingsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "prototypeCell", for: indexPath)
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "prototypeCell", for: indexPath)
 
-        let thing = things[indexPath.row]
+        let thing = filteredThings[indexPath.row]
         
         if let name = thing.name {
             cell.textLabel?.text = name
@@ -78,49 +84,22 @@ class ThingsTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredThings = searchText.isEmpty ? things : things.filter {
+            (item: Thing) -> Bool in
+            return item.name!.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
